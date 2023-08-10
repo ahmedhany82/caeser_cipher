270eb0533edc5af3076a03febb4a296ee08a8c82
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2023-08-10 20:14:28 +0200

    update main for brute force approach

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include "find_key.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char *message = "sbe bhe grnz oneorphr!\n"
+                    "Jura qbrf vg fgneg naq funyy V oevat fbzrguvat?\n"
+                    "Qba'g sbetrg gur oernq sbe gur cnegl.";
+
+    /* Figure out the key by brute-force approach */
+    find_key(message);
     return 0;
 }
